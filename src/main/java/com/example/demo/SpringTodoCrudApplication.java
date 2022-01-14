e6860d7157ce85b720dc2ad5fb6345c2f313f0f9
Shehroz Ali <sshehroza13@gmail.com>
2022-01-14 13:55:34 +0500

    Initialized Spring project

---
+++ src/main/java/com/example/demo/SpringTodoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoCrudApplication.class, args);
+	}
+
+}
